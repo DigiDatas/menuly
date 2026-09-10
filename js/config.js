55c6b753be998c1c8d7edee87ca527cbbe341a9b
DigiDatas <riyasmundengara2@gmail.com>
2026-09-11 01:28:47 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,3 @@
-
+const SUPABASE_URL = "https://vhrxsjfblldksnalpyio.supabase.co"; 
+        const SUPABASE_ANON_KEY = "sb_publishable_we3X4oxc_1R2mQVtlSuJVA_gGcQoQ-k"; 
+        const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
